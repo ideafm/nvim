7dced3a44dd4bb9e2db287e50c96030e51c868b8
Twist <mironuk.s.v@media5.com>
2025-06-20 10:16:58 +0300

    fixes

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,36 +1,42 @@
--- ~/.config/nvim/lua/plugins/lsp.lua (ФИНАЛЬНАЯ ВЕРСИЯ С МОСТОМ)
+-- ~/.config/nvim/lua/plugins/lsp.lua (САМЫЙ ПРОСТОЙ И НАДЕЖНЫЙ ПОДХОД)
 
 return {
-  -- Установщик (LSP, Линтеры, Форматтеры)
+  -- 1. Установщик: Mason
+  -- Его единственная задача - скачать нужные пакеты.
   {
     "williamboman/mason.nvim",
     opts = {
+      -- Указываем имена пакетов, как их знает Mason
       ensure_installed = {
-        "lua-language-server", "typescript-language-server", "css-lsp", "html-lsp", "emmet-ls", "pyright", "vue-language-server", "eslint-lsp",
-        "stylua", "prettier", "black", "isort", "eslint_d",
+        "lua-language-server",
+        "typescript-language-server",
+        "css-lsp",
+        "html-lsp",
+        "emmet-ls",
+        "pyright",
+        "vue-language-server", -- имя в Mason
+        "eslint-lsp",
       },
     },
   },
 
-  -- =========================================================================
-  -- ШАГ 1: ДОБАВЛЯЕМ ПЛАГИН-МОСТ
-  -- Он должен быть в списке, чтобы lazy.nvim его установил.
+  -- 2. Плагин-мост: mason-lspconfig
+  -- Его ЕДИНСТВЕННАЯ задача - сообщить lspconfig, где лежат бинарники Mason.
+  -- Мы НЕ будем вызывать никаких его функций типа setup или setup_handlers.
+  -- Просто его наличие в списке плагинов уже выполняет нужную работу.
   { "williamboman/mason-lspconfig.nvim" },
-  -- =========================================================================
 
-  -- Автодополнение
+  -- 3. Автодополнение (без изменений)
   {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" },
     config = function()
-      -- Ваша конфигурация nvim-cmp остается без изменений
       local cmp = require("cmp")
       cmp.setup({
         snippet = { expand = function(args) require("luasnip").lsp_expand(args.body) end },
         mapping = cmp.mapping.preset.insert({
           ["<C-k>"] = cmp.mapping.select_prev_item(), ["<C-j>"] = cmp.mapping.select_next_item(),
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4), ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(), ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({ { name = "nvim_lsp" }, { name = "luasnip" }, { name = "buffer" }, { name = "path" } }),
@@ -38,10 +44,11 @@ return {
     end,
   },
 
-  -- Главный оркестратор LSP
+  -- 4. Главный оркестратор LSP
+  -- Его задача - настроить серверы, которые он может найти.
   {
     "neovim/nvim-lspconfig",
-    dependencies = { "williamboman/mason.nvim" }, -- mason-lspconfig не нужно указывать здесь, lazy сам разберется
+    dependencies = { "williamboman/mason-lspconfig.nvim" },
     config = function()
       local on_attach = function(client, bufnr)
         if client.name == "ts_ls" or client.name == "volar" then
@@ -50,46 +57,34 @@ return {
         local keymap = vim.keymap
         local opts = { noremap = true, silent = true, buffer = bufnr }
         keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Перейти к определению" }))
-        keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "Показать использования" }))
         keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Показать документацию" }))
-        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "[R]e[n]ame" }))
-        keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "[C]ode [A]ction" }))
+        keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Действия с кодом" }))
       end
       
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      
-      -- =========================================================================
-      -- ШАГ 2: ИСПОЛЬЗУЕМ МОСТ ДЛЯ АВТОМАТИЧЕСКОЙ НАСТРОЙКИ
-      local mason_lspconfig = require("mason-lspconfig")
 
-      -- Указываем, какие серверы мы хотим настроить.
-      -- Имена должны совпадать с теми, что в `ensure_installed` у Mason.
-      local servers = {
-        "lua_ls", "ts_ls", "cssls", "html", "emmet_ls", "pyright", "volar", "eslint"
-      }
+      -- Список серверов для настройки. Имена здесь - как их знает lspconfig.
+      local servers = { "lua_ls", "ts_ls", "cssls", "html", "emmet_ls", "pyright", "volar", "eslint" }
 
-      -- Настраиваем автоматическую установку обработчиков для каждого сервера.
-      mason_lspconfig.setup({
-        ensure_installed = servers,
-      })
-
-      -- Проходим по каждому серверу и применяем наши общие настройки (on_attach, capabilities)
+      -- Простой цикл для настройки каждого сервера
       for _, server_name in ipairs(servers) do
-        local server_opts = {
+        local opts = {
           on_attach = on_attach,
           capabilities = capabilities,
         }
-        -- Добавляем специфичные настройки для конкретных серверов, если нужно
+        
+        -- Применяем специальные настройки, если они нужны
         if server_name == "lua_ls" then
-          server_opts.settings = { Lua = { diagnostics = { globals = { "vim" } } } }
+          opts.settings = { Lua = { diagnostics = { globals = { "vim" } } } }
         end
         if server_name == "volar" then
-          server_opts.filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+          opts.filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
         end
-        lspconfig[server_name].setup(server_opts)
+
+        -- Вызываем стандартную настройку lspconfig
+        lspconfig[server_name].setup(opts)
       end
-      -- =========================================================================
     end,
   },
 }
