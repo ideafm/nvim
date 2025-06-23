478e2e45adbfc2e537a2c84bc47b47fa19c2f0eb
Twist <mironuk.s.v@media5.com>
2025-06-23 18:12:11 +0300

    no message

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,12 +1,9 @@
--- ~/.config/nvim/lua/plugins/lsp.lua (САМЫЙ ПРОСТОЙ И НАДЕЖНЫЙ ПОДХОД)
-
 return {
   -- 1. Установщик: Mason
-  -- Его единственная задача - скачать нужные пакеты.
+  -- Имена пакетов в Mason не меняются.
   {
     "williamboman/mason.nvim",
     opts = {
-      -- Указываем имена пакетов, как их знает Mason
       ensure_installed = {
         "lua-language-server",
         "typescript-language-server",
@@ -14,19 +11,13 @@ return {
         "html-lsp",
         "emmet-ls",
         "pyright",
-        "vue-language-server", -- имя в Mason
+        "vue-language-server",
         "eslint-lsp",
       },
     },
   },
 
-  -- 2. Плагин-мост: mason-lspconfig
-  -- Его ЕДИНСТВЕННАЯ задача - сообщить lspconfig, где лежат бинарники Mason.
-  -- Мы НЕ будем вызывать никаких его функций типа setup или setup_handlers.
-  -- Просто его наличие в списке плагинов уже выполняет нужную работу.
-  { "williamboman/mason-lspconfig.nvim" },
-
-  -- 3. Автодополнение (без изменений)
+  -- 2. Автодополнение (без изменений)
   {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
@@ -36,55 +27,63 @@ return {
       cmp.setup({
         snippet = { expand = function(args) require("luasnip").lsp_expand(args.body) end },
         mapping = cmp.mapping.preset.insert({
-          ["<C-k>"] = cmp.mapping.select_prev_item(), ["<C-j>"] = cmp.mapping.select_next_item(),
-          ["<C-Space>"] = cmp.mapping.complete(), ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({ { name = "nvim_lsp" }, { name = "luasnip" }, { name = "buffer" }, { name = "path" } }),
       })
     end,
   },
 
-  -- 4. Главный оркестратор LSP
-  -- Его задача - настроить серверы, которые он может найти.
+  -- 3. Главный оркестратор LSP
   {
     "neovim/nvim-lspconfig",
-    dependencies = { "williamboman/mason-lspconfig.nvim" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "hrsh7th/nvim-cmp",
+    },
     config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
       local on_attach = function(client, bufnr)
+        -- ИЗМЕНЕНИЕ 1: Обновляем имя в проверке
         if client.name == "ts_ls" or client.name == "volar" then
            client.server_capabilities.documentFormattingProvider = false
         end
         local keymap = vim.keymap
         local opts = { noremap = true, silent = true, buffer = bufnr }
-        keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Перейти к определению" }))
-        -- keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Показать документацию" }))
-        keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Действия с кодом" }))
+        keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "LSP: Перейти к определению" }))
+        keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "LSP: Показать документацию" }))
+        keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "LSP: Действия с кодом" }))
+        keymap.set("n", "gr", vim.lsp.buf.references, vim.tbl_extend("force", opts, { desc = "LSP: Показать ссылки" }))
+        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "LSP: Переименовать" }))
       end
-      
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
 
-      -- Список серверов для настройки. Имена здесь - как их знает lspconfig.
-      local servers = { "lua_ls", "ts_ls", "cssls", "html", "emmet_ls", "pyright", "volar", "eslint" }
+      local default_setup = {
+        on_attach = on_attach,
+        capabilities = capabilities,
+      }
 
-      -- Простой цикл для настройки каждого сервера
-      for _, server_name in ipairs(servers) do
-        local opts = {
-          on_attach = on_attach,
-          capabilities = capabilities,
-        }
-        
-        -- Применяем специальные настройки, если они нужны
-        if server_name == "lua_ls" then
-          opts.settings = { Lua = { diagnostics = { globals = { "vim" } } } }
-        end
-        if server_name == "volar" then
-          opts.filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
-        end
+      -- ИЗМЕНЕНИЕ 2: Обновляем имя сервера при вызове setup
+      lspconfig.ts_ls.setup(default_setup)
+      lspconfig.cssls.setup(default_setup)
+      lspconfig.html.setup(default_setup)
+      lspconfig.emmet_ls.setup(default_setup)
+      lspconfig.pyright.setup(default_setup)
+      lspconfig.eslint.setup(default_setup)
+      lspconfig.volar.setup(default_setup)
 
-        -- Вызываем стандартную настройку lspconfig
-        lspconfig[server_name].setup(opts)
-      end
+      lspconfig.lua_ls.setup(vim.tbl_deep_extend("force", default_setup, {
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+            workspace = { checkThirdParty = false },
+          },
+        },
+      }))
     end,
   },
 }
